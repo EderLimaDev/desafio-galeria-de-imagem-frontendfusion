72d76991736e4331853373da8d1c5b58a98eb520
EderLimaDev <ederlimarh@gmail.com>
2024-10-18 17:10:42 -0300

    chore: add context to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Card } from "./components/Card/Card";
-
+import { useContext } from "react";
+import { ApiContext } from "./context/ApiContext";
 
 function App() {
+
+  const {images} = useContext(ApiContext);
 
   return (
     <>
@@ -9,7 +12,31 @@ function App() {
         <h1 className='text-3xl text-red-400'>Seja bem vindo a Galeria de Imagens!</h1>
         <p>Desafio de Galeria de Imagens - Processo Seletivo Frontend Fusion</p>
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
-          <Card />
+         
+        {
+        images.length > 0 ? (
+            <>
+                {images.map((image) => (
+
+                 <Card
+                    key={image.id}
+                    author={image.author}
+                    width={image.width}
+                    height={image.height}
+                    download_url={image.download_url}
+                   /> 
+
+                ))}
+            </> 
+
+            ) : (
+            <div>Carregando....</div>
+        )
+      } 
+         
+         
+         
+          
         </section>
       </main>
     </>
---
+++ src/context/ApiContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState, useEffect } from "react";
+
+export const ApiContext = createContext()
+
+export const ApiProvider = ({children}) => {
+
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        const url = 'https://picsum.photos/v2/list'
+
+        async function getImages() {
+            await fetch(url)
+            .then((items) =>  items.json())
+            .then((result) => setImages(result))
+        }
+          
+        getImages()
+
+    }, [])
+
+    return (
+        <ApiContext.Provider value={{images}}>{children}</ApiContext.Provider>
+    )
+};
